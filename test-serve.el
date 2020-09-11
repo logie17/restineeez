62040902b8eccfd193cfd81f135032b495d82425
Logan Bell <loganbell@gmail.com>
2020-09-10 21:39:39 -0700

    test file

---
+++ test-serve.el
@@ -0,0 +1,10 @@
+(require 'test-simple)
+(test-simple-start) ;; Zero counters and start the stop watch.
+
+;; Use (load-file) below because we want to always to read the source.
+;; Also, we don't want no stinking compiled source.
+(assert-t (load-file "./serve2.el")
+	  "Can't load serve.el - are you in the right directory?" )
+
+
+(end-tests) ;; Stop the clock and print a summary
